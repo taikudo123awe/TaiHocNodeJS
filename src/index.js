ac928cda6f020f3a6f0d5d2fe99af95e681a27f2
TaiTran <tantai2976@gmail.com>
2025-07-16 18:10:46 +0700

    Move index.js to src/, update views and partials

---
+++ src/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const { engine } = require('express-handlebars'); 
+const path = require('path');
+const app = express()
+const port = 3000
+
+app.engine('hbs', engine(
+  {extname: '.hbs'
+   // 👉 đây là layout mặc định
+  }
+)); // ✅ Gọi hàm engine()
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resource\\views'));
+
+
+app.get('/', (req, res) => {
+  res.render('home');
+    
+})
+app.get('/tin-tuc', (req, res) => {
+  res.render('news');
+    
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`)
+
+})
